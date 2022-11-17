b9f2d213859e3468603a8f9d85fee0ec837431e0
sindhujaalladi <sindhu.alladi1993@gmail.com>
2022-11-16 19:09:29 -0600

    inital commit

---
+++ src/main/java/com/springsecuritywithmysqldb/SpringSecurityWithMysqlDbApplication.java
@@ -0,0 +1,13 @@
+package com.springsecuritywithmysqldb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithMysqlDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithMysqlDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsecuritywithmysqldb/SpringSecurityWithMysqlDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecuritywithmysqldb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithMysqlDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
